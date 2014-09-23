6a8a73256ae09b69fb6aaa9f163f797584e2a93f
Lee SeongIn <poison0107@hanmail.net>
2014-09-23 12:00:03 +0900

    Why So Serious
    Why So Serious

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ public Class Test{
     public static void main(String[] args){
         System.out.println("we are the mad-sheep");
         System.out.println("and you are a lamb");
+        System.out.println("Why So Serious");
     }
 
 }
